dcaa9f07a1c2aca7536b629a73c3ed93e9971b2b
clumpmore <ivershinin21@gmail.com>
2023-08-23 20:50:50 +0300

    added packages and domain logic №2

--- app/src/main/java/com/example/shoppinglist/domain/AddItemUseCase.kt
+++
@@ -1,7 +0,0 @@
-package com.example.shoppinglist.domain
-
-class AddItemUseCase {
-    fun addItem(shopItem: ShopItem) {
-        TODO()
-    }
-}
--- app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.shoppinglist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.shoppinglist.R
+import com.example.shoppinglist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.shopList.observe(this) {
+            Log.d(TAG, it.toString())
+            if (count==0) {
+                count++
+                val item = it[0]
+                viewModel.changeEnabledState(item)
+            }
+
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
